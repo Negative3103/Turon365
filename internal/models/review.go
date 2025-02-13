9091162635e60da330e9ba962919e593e297ceab
Khasan Davronbekov <xasand3103@gmail.com>
2025-02-13 15:44:55 +0500

    reviews

--- internal/models/review.go
+++ internal/models/review.go
@@ -7,7 +7,7 @@ import (
 
 type Review struct {
 	ID        uuid.UUID `db:"id"`
-	JobID     uuid.UUID `db:"job_id"`
+	JobID     uuid.UUID `db:"job_id" json:"job_id"`
 	Rating    int       `db:"rating"`
 	Comment   string    `db:"comment"`
 	CreatedAt time.Time `db:"created_at"`
